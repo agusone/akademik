ee6c5363310b0167c036eb36df04b1367116c15b
Pt.Agus setiawan <agus63098@gmail.com>
2019-07-03 11:37:52 +0800

    update db

--- db/akademik.sql
+++ db/akademik.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 03, 2019 at 03:49 AM
+-- Generation Time: Jul 03, 2019 at 05:36 AM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 7.2.12
 
@@ -43,7 +43,10 @@ CREATE TABLE `data_jadwal` (
 INSERT INTO `data_jadwal` (`id`, `matkul`, `jam`, `kelas`, `ruangan`) VALUES
 (1, 'Perancangan Web', 'senin,13:00 - 14:00', 'ai173', 'IV.3'),
 (2, 'kalkulus', 'selasa, 13:00 - 15:00', 'ai173', 'V.4'),
-(5, 'Bahasa Inggris', 'selasa, 13:00 - 14:00', 'ai173', 'V.1');
+(5, 'Bahasa Inggris', 'selasa, 13:00 - 14:00', 'ai173', 'V.1'),
+(6, 'Bahasa Inggris', 'selasa, 13:00 - 14:00', 'ac179', 'Lab Mobile'),
+(7, 'Java ', 'rabu, 13:00 - 14:00', 'ad170', 'III.4'),
+(8, 'MObile', 'selasa, 13:00 - 14:00', 'ac179', 'III.4');
 
 -- --------------------------------------------------------
 
@@ -74,7 +77,8 @@ CREATE TABLE `data_mahasiswa` (
 
 INSERT INTO `data_mahasiswa` (`id`, `nim`, `nama`, `phone`, `kelas`, `alamat`, `email`, `jenis_kelamin`, `agama`, `tmp_lahir`, `tgl_lahir`, `angkatan`, `prodi`, `img`) VALUES
 (6, '170030258', ' Putu Agus Setiawan', '6309214861', 'ai173', 'Tegalalang Gianyar', 'akunini63098@gmail.com', 'Laki - Laki', 'Budha', 'gianyar', '1998-02-20', '2017', 'Sistem Informasi', 'gianyar.jpg'),
-(7, '123123123', 'Ketut Budiarta', '82247148890', 'ac179', 'Tengkulak kaja', 'akunini63098@gmail.com', 'Lainnya', 'Kong Hu Cu', 'bangli', '1222-12-21', '2017', 'Sistem Informasi', 'buleleng.jpg');
+(7, '123123123', 'Ketut Budiarta', '82247148890', 'ac179', 'Tengkulak kaja', 'akunini63098@gmail.com', 'Lainnya', 'Kong Hu Cu', 'bangli', '1222-12-21', '2017', 'Sistem Informasi', 'sandiaga-uno-3.jpg'),
+(8, '231234213', 'Kommang bayu kurniawan', '82247148890', 'ai173', 'Tegalalang Gianyar', 'Agus63098@gmail.com', 'Perempuan', 'Islam', 'gianyar', '0000-00-00', '2018', 'Sistem Informasi', 'IMG_1900.jpg');
 
 -- --------------------------------------------------------
 
@@ -84,7 +88,7 @@ INSERT INTO `data_mahasiswa` (`id`, `nim`, `nama`, `phone`, `kelas`, `alamat`, `
 
 CREATE TABLE `user` (
   `id` int(11) NOT NULL,
-  `nim` int(225) NOT NULL,
+  `kode_user` varchar(225) COLLATE utf8mb4_unicode_ci NOT NULL,
   `username` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `level` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL
@@ -94,10 +98,12 @@ CREATE TABLE `user` (
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `nim`, `username`, `password`, `level`) VALUES
-(7, 123, 'admin', '123', 'admin'),
-(10, 170030258, 'agus', '123', 'mahasiswa'),
-(11, 123123123, 'budi', '123', 'mahasiswa');
+INSERT INTO `user` (`id`, `kode_user`, `username`, `password`, `level`) VALUES
+(7, '123', 'admin', '123', 'admin'),
+(10, '170030258', 'agus', '123', 'mahasiswa'),
+(11, '123123123', 'budi', '123', 'mahasiswa'),
+(12, 'Kadek Hendra', 'admin1', '123', 'admin'),
+(13, '231234213', 'nyoman', '123', 'mahasiswa');
 
 --
 -- Indexes for dumped tables
@@ -129,19 +135,19 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `data_jadwal`
 --
 ALTER TABLE `data_jadwal`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `data_mahasiswa`
 --
 ALTER TABLE `data_mahasiswa`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
